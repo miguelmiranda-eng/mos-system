7497d589634b646c5cb7ff272f2d73f255075bc8
Miguel Miranda <miguel.miranda@prosper-mfg.com>
2026-04-14 19:43:08 -0700

    fix: unify theme providers using local ThemeContext to fix Dashboard crash

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,8 @@ import { useLang } from "./contexts/LanguageContext";
 import { Loader2, Mail, Lock, Eye, EyeOff, ArrowLeft } from "lucide-react";
 import { toast } from "sonner";
 import { Toaster } from "./components/ui/sonner";
-import { ThemeProvider } from "next-themes";
+import { ThemeProvider } from "./contexts/ThemeContext";
+
 
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
@@ -697,8 +698,9 @@ import { LanguageProvider } from "./contexts/LanguageContext";
 function App() {
   return (
     <BrowserRouter>
-      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+      <ThemeProvider>
         <AuthProvider>
+
           <LanguageProvider>
             <AppRouter />
           </LanguageProvider>
